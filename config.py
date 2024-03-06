628479b85f8662b675e9d5958589daf410041811
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-03-06 10:48:38 -0300

    refactor: criada função para coletar db uri

--- config.py
+++ config.py
@@ -2,7 +2,16 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+def get_db_uri():
+    user = os.environ.get("DB_USER")
+    password = os.environ.get("DB_PASSWORD")
+    host = os.environ.get("DB_HOST")
+    database = os.environ.get("DB_DATABASE")
+    port = os.environ.get("DB_PORT")
+
+    return f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres.egnmntyafgtdjwdhmtbz:Tz8a4oIdPcPArvuO@aws-0-sa-east-1.pooler.supabase.com:6543/postgres'
+SQLALCHEMY_DATABASE_URI = get_db_uri()
 SQLALCHEMY_TRACK_MODIFICATIONS = False
